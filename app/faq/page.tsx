f1de513cd385232eb3f0d091a52344681e3b2717
Aysun Baghirzada <aysun.baghirzada@gmail.com>
2025-11-21 18:10:10 +0400

    Add Banner component with title, subtitle, and icon support

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,7 +1,20 @@
+import Banner from "@/components/common/banner"
 
 const Faq = () => {
   return (
-    <div>Faq</div>
+    <div>
+      <Banner
+        title="FAQs"
+        subtitle={
+          <>
+            <span className="text-primary-700">FitNest</span>
+            <span> haqqında ən çox soruşulan suallar</span>
+          </>
+        }
+        iconUrl="/public/icons/mail.svg"
+        iconAlt="FAQ Icon"
+      />
+    </div>
   )
 }
 
--- components/common/banner.tsx
+++ components/common/banner.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const banner = () => {
-  return (
-    <div>banner</div>
-  )
+interface BannerProps {
+    title: string;
+    subtitle: string | React.ReactNode;
+    iconUrl: string;
+    iconAlt?: string;
 }
 
-export default banner
+const Banner: React.FC<BannerProps> = ({ title, subtitle, iconUrl, iconAlt }) => {
+    return (
+        <div className='absolute flex justify-center left-0 pt-20 pb-[173px] top-[193px] text-white bg-blue-500 w-full bg-linear-to-l from-[#243665] to-[#0B1223]'>
+            <div>
+                <div className='flex items-center gap-3'>
+                    <img src={iconUrl} alt={iconAlt} />
+                    <h2 className='text-h2-size leading-h2 font-medium'>{title}</h2>
+                </div>
+                <p className='text-s2-size leading-s2 font-medium mt-3'>{subtitle}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
