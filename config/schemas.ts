98eba6714a65e408fc6eb9f6d56bfbfc9fd2a6d1
Farmayil001 <gasanoff8085@gmail.com>
2025-11-25 14:40:53 +0400

    Teklif ve sikayetler

--- config/schemas.ts
+++ config/schemas.ts
@@ -14,3 +14,17 @@ export const contactFormSchema = z.object({
     .min(2, { message: "Mesaj ən azı 2 simvol olmalıdır" })
     .max(500, { message: "Mesaj ən çox 500 simvol olmalıdır" }),
 });
+
+export const feedbackSchema = z.object({
+  fullname: z.string()
+    .min(2, { message: "Ad soyad ən azı 2 simvol olmalıdır" })
+    .max(50, { message: "Ad soyad ən çox 50 simvol olmalıdır" }),
+  email: z.string()
+    .min(1, { message: "Email ünvanı tələb olunur" })
+    .email({ message: "Düzgün email ünvanı daxil edin" }),
+  type: z.string()
+    .min(1, { message: "Feedback növü tələb olunur" }),
+  message: z.string()
+    .min(2, { message: "Mesaj ən azı 2 simvol olmalıdır" })
+    .max(500, { message: "Mesaj ən çox 500 simvol olmalıdır" }),
+});
