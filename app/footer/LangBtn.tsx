fb793cba926057b93883bb4f444a2f42cd7d2164
Aysun Baghirzada <aysun.baghirzada@gmail.com>
2025-12-05 16:27:31 +0400

    Refactor gym page and LangBtn component for improved structure and functionality

--- app/footer/LangBtn.tsx
+++ app/footer/LangBtn.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-const LangBtn = ({ txt ,isActive}: { txt: string, isActive?: boolean }) => {
-  return (
+interface LangBtnProps {
+  txt: string;
+  isActive?: boolean;
+  className?: string;
+  enableClick?: boolean; 
+}
+
+const LangBtn = ({
+  txt,
+  isActive,
+  className,
+  enableClick = true,}: LangBtnProps) => {
+  const handleClick = () => {
+    if (!enableClick) return; 
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+    return (
     <button
-      onClick={() => window.scrollTo({ top: 0 })}
-      className={`bg-linear-to-r ${isActive ? "from-[#C6A7F5] to-[#01A2BC]" : "from-[#7c8da0] via-0% to-[#7c8da0]"}  text-neutral-50 text-b1 leading-b1 font-medium  p-px  rounded-full cursor-pointer w-10 h-10`}
+      onClick={handleClick}
+      className={`bg-linear-to-r ${isActive ? "from-[#C6A7F5] to-[#01A2BC]" : "from-[#7c8da0] via-0% to-[#7c8da0]"}  text-neutral-50 text-b1 leading-b1 font-medium  p-px  rounded-full cursor-pointer w-10 h-10 ${className}`}
     >
-      <span className="flex w-full bg-softblue-950 text-white rounded-full p-2  ">
+      <span className="flex justify-center items-center w-full bg-softblue-950 text-white rounded-full p-2  ">
         {txt}
       </span>
     </button>
