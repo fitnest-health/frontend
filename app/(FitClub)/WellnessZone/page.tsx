006ae07ed7062b0707b2122397ad9abdb5275ec4
Ichihiroy <realyanisquliyev@gmail.com>
2026-01-07 01:52:29 +0400

    Refactor card component styles and improve formatting in WellnessZone page

--- app/(FitClub)/WellnessZone/page.tsx
+++ app/(FitClub)/WellnessZone/page.tsx
@@ -3,26 +3,44 @@ import Image from "next/image";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 
 const boxes = [
-  { title: "Stressin azalması", text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir." },
-  { title: "Enerji balansı", text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir." },
-  { title: "Zehni aydınlıq", text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir." },
-  { title: "Özünü yaxşı hiss et", text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir." },
+  {
+    title: "Stressin azalması",
+    text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir.",
+  },
+  {
+    title: "Enerji balansı",
+    text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir.",
+  },
+  {
+    title: "Zehni aydınlıq",
+    text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir.",
+  },
+  {
+    title: "Özünü yaxşı hiss et",
+    text: "SPA və yoga seansları ilə gündəlik gərginlikdən azad ol, zehnini sakitləşdir.",
+  },
 ];
 
 const page = () => {
   return (
     <div className="lg:max-w-7xl lg:mx-auto lg:mt-20">
-
       <div className="md:pt-10">
         <div className="relative mx-auto overflow-hidden rounded-4xl w-[360px] h-[447px] md:w-[728px] md:h-[449px] lg:max-w-7xl lg:w-full lg:h-[582px] sm:w-full">
-          <Image src="/images/masaj.png" alt="image" fill className="object-cover" />
+          <Image
+            src="/images/masaj.png"
+            alt="image"
+            fill
+            className="object-cover"
+          />
           <div className="absolute bottom-0 w-full h-2/4 bg-softblue-950/70 p-6 rounded-4xl lg:flex lg:items-center lg:justify-between lg:h-1/4 lg:pt-10">
             <div>
               <h2 className="text-s1 leading-s1 font-bold text-neutral-50 md:text-h6 md:leading-h6">
                 Bədənini və zehnini rahatlaşdır
               </h2>
               <p className="mt-4 text-b3 leading-b3 text-neutral-50 md:text-s2 md:leading-s2 md:mb-9">
-                Gərginlikdən uzaqlaş, <span className="text-primary-700">FitNest</span>-in SPA və yoga proqramları ilə balansını yenidən qur.
+                Gərginlikdən uzaqlaş,{" "}
+                <span className="text-primary-700">FitNest</span>-in SPA və yoga
+                proqramları ilə balansını yenidən qur.
               </p>
             </div>
             <div className="mt-9 mb-6 lg:min-w-[306px] lg:mb-10 md:mt-2">
@@ -37,32 +55,33 @@ const page = () => {
             Bu proqramların sənə qazandıracağı rahatlıq
           </h4>
           <p className="mt-2 text-neutral-50 font-medium text-b2 leading-b2 md:text-s2 md:leading-s2">
-            Hər seans bədənini yeniləyir, zehnini sakitləşdirir və gündəlik balansını qorumağa kömək edir.
+            Hər seans bədənini yeniləyir, zehnini sakitləşdirir və gündəlik
+            balansını qorumağa kömək edir.
           </p>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-6 items-stretch mb-14">
-          <div className="relative w-full mx-auto max-w-[728px] aspect-square lg:max-w-[632px] lg:aspect-square overflow-hidden rounded-4xl">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-6 items-stretch mb-14 px-4 xl:px-0">
+          <div className="relative w-full mx-auto max-w-[728px] aspect-square lg:max-w-[632px] overflow-hidden rounded-4xl h-[300px]  md:h-screen ">
             <Image
               src="/images/wellness.png"
               alt="image"
               fill
-              className="object-center"
+              className="object-cover bg-black opacity-70"
             />
           </div>
-          <div className="grid grid-cols-1 gap-5 mt-6 lg:mt-0 max-w-[624px] w-full">
+          <div className="grid grid-cols-1 gap-5 mt-6 lg:mt-0 max-w-[624px] w-full h-screen mx-auto lg:max-w-none lg:mx-0">
             {boxes.map((box, index) => (
-              <Card key={index} className="min-h-[194px]">
-                <CardContent className="px-5 py-6">
-                  <div className="mb-2 flex h-10 w-10 items-center justify-center rounded-full gradient-border font-semibold text-white text-s2 leading-s2 sm:text-t2 sm:leading-b1">
+              <Card key={index} className="min-h-[120px]">
+                <div className="absolute top-5 left-5">
+                  <div className="mb-2 flex h-10 w-10 items-center justify-center rounded-full gradient-border  text-white text-s2 leading-s2 sm:text-t2 sm:leading-b1 font-normal">
                     {index + 1}
                   </div>
-                  <CardTitle className="mt-6 md:mt-8 text-white text-b1 leading-b1 sm:text-s1 sm:leading-s1 font-semibold">
+                  <CardTitle className="mt-6 md:mt-8  text-white text-b1 leading-b1 sm:text-s1 sm:leading-s1 font-semibold">
                     {box.title}
                   </CardTitle>
-                  <p className="mt-3 text-b2 leading-b3 sm:text-b1 sm:leading-b1 text-gray-400">
+                  <p className="text-b2 leading-b3 sm:text-b1 sm:leading-b1 text-gray-400">
                     {box.text}
                   </p>
-                </CardContent>
+                </div>
               </Card>
             ))}
           </div>
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,31 +1,24 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card"
       className={cn(
-        "w-full py-8 px-9 flex flex-col items-start gap-4 rounded-4xl gradient-border bg-card shadow-[0px_5px_10px_0px_rgba(0,172,204,0.3)] text-neutral-400",
+        "w-full py-8 px-6 flex flex-col items-start gap-4 rounded-4xl gradient-border bg-card shadow-[0px_5px_10px_0px_rgba(0,172,204,0.3)] text-neutral-400",
         className
       )}
       {...props}
     />
-  )
+  );
 }
 
 function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-header"
-      className={cn(
-        "",
-        className
-      )}
-      {...props}
-    />
-  )
+    <div data-slot="card-header" className={cn("", className)} {...props} />
+  );
 }
 
 function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
@@ -35,21 +28,12 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
       className={cn("text-card-foreground", className)}
       {...props}
     />
-  )
+  );
 }
 function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-content"
-      className={cn("", className)}
-      {...props}
-    />
-  )
+    <div data-slot="card-content" className={cn("", className)} {...props} />
+  );
 }
 
-export {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-}
+export { Card, CardHeader, CardTitle, CardContent };
